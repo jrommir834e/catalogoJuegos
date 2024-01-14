da131951e60177519b944a9aee00e03294548354
jesus romo <jrommir834e@g.educaand.es>
2024-01-14 12:26:43 +0100

    terminado añadiento otro catalogo que se me habia olvidado

--- src/main/java/daw/CatalogoJuegos.java
+++ src/main/java/daw/CatalogoJuegos.java
@@ -9,7 +9,6 @@ import java.util.Comparator;
 import java.util.Iterator;
 import java.util.Objects;
 
-
 /**
  *
  * @author jesus
@@ -19,92 +18,95 @@ public class CatalogoJuegos {
     public static void main(String[] args) {
 
         Juegos j1 = new Juegos("Super Mario", 1990, 9, "nintendo", "nintendo");
-        
-       CatalogoJuegos catalogo1= new CatalogoJuegos();
+
+        CatalogoJuegos catalogo1 = new CatalogoJuegos();
+        CatalogoJuegos catalogo2 = new CatalogoJuegos();
 
         //Método 1
         System.out.println("Método 1");
         System.out.println("El catálogo tiene " + catalogo1.numeroElementos()
                 + " elementos");
+        System.out.println("El catálogo 2 tiene " + catalogo2.numeroElementos()
+                + " elementos");
 
-      
         //metodo2
-       System.out.println("\nMétodo 2");
-       System.out.println("¿Está vacío el catálogo? " + catalogo1.estaVacio());
-        
+        System.out.println("\nMétodo 2");
+        System.out.println("¿Está vacío el catálogo? " + catalogo1.estaVacio());
+
         //Método 3
         System.out.println("\nMétodo 3");
         catalogo1.guardarElemento(j1);
-        catalogo1.guardarElemento(new Juegos("playstation", 
+        catalogo1.guardarElemento(new Juegos("playstation",
                 1992, 18, "maniark", "yakjito"));
-      
-        
-         //Método 4
+
+        catalogo2.guardarElemento(new Juegos("nintendogs",
+                1599, 15, "saijhsajs", "nomammaa"));
+        catalogo2.guardarElemento(new Juegos("xbox",
+                1892, 50, "maniadadark", "saoijsa"));
+        //Método 4
         System.out.println("\nMétodo 4");
         System.out.println(catalogo1.verElemento(0));
         System.out.println(catalogo1.verElemento(1));
-        
-        
-         //Método 5
+        System.out.println(catalogo2.verElemento(0));
+        System.out.println(catalogo2.verElemento(1));
+
+        //Método 5
         System.out.println("\nMétodo 5");
         catalogo1.cambiarElemento(0, new Juegos());
         System.out.println(catalogo1.verElemento(0));
-        
+
         //Método 6
         System.out.println("\nMétodo 6");
         catalogo1.eliminarElemento(0);
         System.out.println(catalogo1.verElemento(0));
-        
-         //Método 7
+
+        //Método 7
         System.out.println("\nMétodo 7");
-        catalogo1.eliminaElemento(j1);
-        catalogo1.imprimirCatalogo();
- 
-          //Método 8
+        catalogo2.eliminaElemento(j1);
+        catalogo2.imprimirCatalogo();
+
+        //Método 8
         System.out.println("\nMétodo 8");
         ArrayList<Juegos> juegosLista = new ArrayList<>();
-        juegosLista.add(new Juegos("rachet", 1990, 9, 
+        juegosLista.add(new Juegos("rachet", 1990, 9,
                 "xbox", "yakitolakaka"));
-        
+
         juegosLista.add(new Juegos("2345 jfj", 2299, 9,
                 "Enrique iglesia", "ahskw"));
-        catalogo1.eliminarElemento(juegosLista);
-        catalogo1.imprimirCatalogo();
-        
-         //Método 9
+        catalogo2.eliminarElemento(juegosLista);
+        catalogo2.imprimirCatalogo();
+
+        //Método 9
         System.out.println("\nMétodo 9");
-        catalogo1.eliminarTodos();
-        System.out.println("¿Lista 1 está vacía? " + catalogo1.estaVacio());
+        catalogo2.eliminarTodos();
+        System.out.println("¿La lista 2 está vacía? " + catalogo2.estaVacio());
         
         //Método 10
         System.out.println("\nMétodo 10");
         catalogo1.imprimirCatalogo();
-        
-         //Método 11
+
+        //Método 11
         System.out.println("\nMétodo 11");
-        System.out.println("el juego 1 se encuentra en " + 
-                catalogo1.buscarElementoIndexOf(j1));
-        
+        System.out.println("el juego 1 se encuentra en "
+                + catalogo1.buscarElementoIndexOf(j1));
+
         //Método 12
         System.out.println("\nMétodo 12");
         catalogo1.ordenarPorAño();
         catalogo1.imprimirCatalogo();
-        
+
         //Método 13
         System.out.println("\nMétodo 13");
-        System.out.println("El objeto j1 está en la posición " + 
-                catalogo1.buscarElementoBinarySearch(j1));
-        
+        System.out.println("El objeto j1 está en la posición "
+                + catalogo1.buscarElementoBinarySearch(j1));
+
     }
-        
-    
+
     private ArrayList<Juegos> listaJuegos;
 
     public CatalogoJuegos() {
         this.listaJuegos = new ArrayList<>();
     }
-
-  
 
     public ArrayList<Juegos> getListaJuegos() {
         return listaJuegos;
@@ -145,21 +147,17 @@ public class CatalogoJuegos {
         return sb.toString();
     }
 
-  
-   
-
     //Creación de Métodos
     //numeroElementos(): devuelve el número de objetos que hay en la lista.
     public int numeroElementos() {
         return listaJuegos.size();
     }
 
-    
     public boolean estaVacio() {
         return listaJuegos.isEmpty();
     }
 
-     public Juegos verElemento(int posicion) {
+    public Juegos verElemento(int posicion) {
         try {
             return listaJuegos.get(posicion);
         } catch (IndexOutOfBoundsException a) {
@@ -169,7 +167,6 @@ public class CatalogoJuegos {
 
     }
 
-    
     public void cambiarElemento(int posicion, Juegos nuevoObjeto) {
         try {
             listaJuegos.set(posicion, nuevoObjeto);
@@ -193,7 +190,6 @@ public class CatalogoJuegos {
         listaJuegos.remove(eliminarObjeto);
     }
 
- 
     public void eliminarElemento(ArrayList<Juegos> eliminarObjetoLista) {
         listaJuegos.removeAll(eliminarObjetoLista);
     }
@@ -210,9 +206,7 @@ public class CatalogoJuegos {
         }
     }
 
-    
     public void imprimirCatalogo() {
-
 
         listaJuegos.forEach(System.out::println);
     }
@@ -222,27 +216,28 @@ public class CatalogoJuegos {
         return listaJuegos.indexOf(elementoABuscar);
     }
 
-    public void ordenarPorCreadores(){
-        Collections.sort(listaJuegos, 
-                (e1,e2)->e1.getCreadores().compareToIgnoreCase(e2.getCreadores()));
+    public void ordenarPorCreadores() {
+        Collections.sort(listaJuegos,
+                (e1, e2) -> e1.getCreadores().compareToIgnoreCase(e2.getCreadores()));
     }
-    public void ordenarPorAño(){
-        Collections.sort(listaJuegos, 
-                (e1,e2)->e1.getAñoLanzamiento()-e2.getAñoLanzamiento());
+
+    public void ordenarPorAño() {
+        Collections.sort(listaJuegos,
+                (e1, e2) -> e1.getAñoLanzamiento() - e2.getAñoLanzamiento());
     }
-    public void ordenarPorPegi(){
-        Collections.sort(listaJuegos, 
-                (e1,e2)->Integer.compare(e1.getPegi(), e2.getPegi()));
+
+    public void ordenarPorPegi() {
+        Collections.sort(listaJuegos,
+                (e1, e2) -> Integer.compare(e1.getPegi(), e2.getPegi()));
     }
-    
-    
-    public int buscarElementoBinarySearch(Juegos elementoABuscar){
+
+    public int buscarElementoBinarySearch(Juegos elementoABuscar) {
         //La lista tiene estar ordenada primero
         //ejecutar primero el método de ordenación
         elementoABuscar.setCreadores("nintendo");
-        return Collections.binarySearch(listaJuegos, 
-                elementoABuscar, 
-                (e1,e2)->e1.getCreadores().compareToIgnoreCase(e2.getCreadores()));
-  
-}
+        return Collections.binarySearch(listaJuegos,
+                elementoABuscar,
+                (e1, e2) -> e1.getCreadores().compareToIgnoreCase(e2.getCreadores()));
+
+    }
 }
